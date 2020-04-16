aaf646086e0941a1dd300c96b8782ff088364c6c
valid <valid.saley@gmail.com>
2020-04-16 18:30:47 +0300

    Security, List-Form-Search views

--- myaddons/initmodule/__manifest__.py
+++ myaddons/initmodule/__manifest__.py
@@ -5,9 +5,13 @@
     'author': 'Valid Saley',
     'licence': 'APGL-3',
     'website': 'http://www.example.com',
-    'category': 'Uncategorized',
+    'category': 'Library',
     'version': '12.0.1.0.0',
     'depends': ['base'],
-    # 'data': ['views.xml'],
+    'data': [
+        'security/library_security.xml',
+        'security/ir.model.access.csv',
+        'views/library_book.xml'
+    ],
     # 'demo': ['demo.xml'],
 }
