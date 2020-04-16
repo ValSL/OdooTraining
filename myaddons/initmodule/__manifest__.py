3c4d6baf672e2c75bc00830c2555f412fa8b65f0
valid <valid.saley@gmail.com>
2020-04-16 15:19:34 +0300

    base structure dir tree

---
+++ myaddons/initmodule/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Ttile',
+    'summary': 'Short subtitle phrase',
+    'description': """Long description""",
+    'author': 'Valid Saley',
+    'licence': 'APGL-3',
+    'website': 'http://www.example.com',
+    'category': 'Uncategorized',
+    'version': '12.0.1.0.0',
+    'depends': ['base'],
+    'data': ['views.xml'],
+    'demo': ['demo.xml'],
+}
