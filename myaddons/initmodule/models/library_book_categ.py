8781f5cd0b66cbd25a51ad90962bf83973b78f79
valid <valid.saley@gmail.com>
2020-04-17 19:15:18 +0300

    Adding a hierarchy to a Model

---
+++ myaddons/initmodule/models/library_book_categ.py
@@ -0,0 +1,27 @@
+from odoo import fields, models, api
+
+
+class BookCategory(models.Model):
+    _name = 'library.book.category'
+    name = fields.Char('Category')
+
+    _parent_store = True
+    parent_left = fields.Integer(index=True)
+    parent_right = fields.Integer(index=True)
+
+    parent_id = fields.Many2one(
+        'library.book.category',
+        string='Parent Category',
+        ondelete='restrict',
+        index=True
+    )
+    child_ids = fields.One2many(
+        'library.book.category',
+        'parent_id',
+        string='Child Categories'
+    )
+
+    @api.constrains
+    def _check_hierarchy(self):
+        if not self._check_recursion():
+            raise models.ValidationError('Error! You cannot create recursive categories.')
