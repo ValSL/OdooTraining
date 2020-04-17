080d97fa14cfe33d204b364a5115a494c673b946
valid <valid.saley@gmail.com>
2020-04-17 15:39:57 +0300

    Add some specific attributes to model fields

--- myaddons/initmodule/models/library_book.py
+++ myaddons/initmodule/models/library_book.py
@@ -4,6 +4,32 @@ from odoo import fields, models
 class LibraryBook(models.Model):
     _name = 'library.book'
     _description = 'Book'
+    _order = 'date_release desc, name'
+    _rec_name = 'short_name'
     name = fields.Char(string='Title', required=True)
+    short_name = fields.Char(string='Short Title', size=100, translate=True)
     date_release = fields.Date(string='Release Date')
+    date_updated = fields.Datetime('Last Updated')
     author_ids = fields.Many2many('res.partner', string='Authors')
+    notes = fields.Text('Internal notes')
+    state = fields.Selection(
+        [('draft', 'Not Available'),
+         ('available', 'Available'),
+         ('lost', 'Lost')],
+        'State')
+    description = fields.Html(string='Description', sanitize=True, strip_style=False, translate=False)
+    out_of_print = fields.Boolean('Out of print?')
+    pages = fields.Integer(
+        string='Number of pages',
+        default=0,
+        help='Total book page count',
+        groups='base.group_user',
+        states={'cancel': [('readonly', True)]},
+        copy=True,
+        index=False,
+        readonly=False,
+        required=False,
+        company_depends=False,
+    )
+    reader_rating = fields.Float('Reader Average Rating', (14, 4))
+
