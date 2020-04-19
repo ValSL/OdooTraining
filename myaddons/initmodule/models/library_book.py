a365a533f40c4633a8c96843a0ca68b63d5d6ebc
valid <valid.saley@gmail.com>
2020-04-19 17:40:18 +0300

    Using delegation inheritance to copy features to another model

--- myaddons/initmodule/models/library_book.py
+++ myaddons/initmodule/models/library_book.py
@@ -127,3 +127,10 @@ class ResPartner(models.Model):
             r.count_books = len(r.author_book_ids)
 
 
+class LibraryMember(models.Model):
+    _name = 'library.member'
+    partner_id = fields.Many2one('res.partner', ondelete='cascade', delegate=True)
+    date_start = fields.Date('Member Since')
+    date_end = fields.Date('Termination Date')
+    member_number = fields.Char()
+    date_of_birth = fields.Date('Date of birth')
